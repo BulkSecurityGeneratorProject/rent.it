802fcb8120e4c05f68448ea34342fba7b402e05f
picaro <picaro.vs@gmail.com>
2016-08-17 13:18:05 +0800

    Readme updates

--- src/main/java/com/op/rentit/domain/Image.java
+++ src/main/java/com/op/rentit/domain/Image.java
@@ -1,6 +1,7 @@
 package com.op.rentit.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -11,9 +12,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.Objects;
 
-/**
- * A Image.
- */
+@Data
 @Entity
 @Table(name = "image")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
@@ -40,73 +39,4 @@ public class Image implements Serializable {
     @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
     private Set<Product> product_images = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Set<Product> getProduct_images() {
-        return product_images;
-    }
-
-    public void setProduct_images(Set<Product> products) {
-        this.product_images = products;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Image image = (Image) o;
-        if(image.id == null || id == null) {
-            return false;
-        }
-        return Objects.equals(id, image.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Image{" +
-            "id=" + id +
-            ", name='" + name + "'" +
-            ", url='" + url + "'" +
-            ", type='" + type + "'" +
-            '}';
-    }
 }
