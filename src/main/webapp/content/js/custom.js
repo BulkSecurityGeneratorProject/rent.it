17baad66c9aaeafb5d962299e041e6b7effa21b4
Oleksandr_Gorbatov <Oleksandr_Gorbatov@epam.com>
2016-08-21 20:19:19 +0300

    added new styles, fonts and images

---
+++ src/main/webapp/content/js/custom.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($){
+
+
+
+
+	$('.navigation').singlePageNav({
+        currentClass : 'active'
+    });
+
+
+
+    $('.flexslider').flexslider({
+	    animation: "fade",
+	    directionNav: false
+	});
+
+
+ 
+
+	$('.menu-toggle-btn').click(function(){
+        $('.responsive-menu').stop(true,true).slideToggle();
+    });
+
+
+
+
+});
